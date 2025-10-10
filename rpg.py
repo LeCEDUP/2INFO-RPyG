7e8c0ea0db05eb6c79f1aaf7a0b58ee64d0712dd
vitoresantos16 <vitoreduardosantosoliveira@gmail.com>
2025-10-10 10:47:38 -0300

    first menu

--- rpg.py
+++ rpg.py
@@ -1,1 +1,19 @@
-# Desenvolva o seu jogo aqui
+# RPG - Vitor Eduardo dos Santos de Oliveira - 2-Info-11
+
+# Importação das classes:
+
+from itens.item import Item
+from itens.armadura import Armadura
+from itens.arma import Arma
+from personagens.heroi import Heroi
+from personagens.monstro import Monstro
+
+# Menu:
+
+print("Bem vindo ao RPyG de Hollow Knight!")
+
+opcao_menu = input("Escolha uma opção: ")
+
+print("1. Iniciar")
+print("2. Criar novo personagem")
+print("3. Sair")

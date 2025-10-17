09a90a3e26f4a3eef4ec20c69ddccbcac4e30651
vitoresantos16 <vitoreduardosantosoliveira@gmail.com>
2025-10-17 07:46:21 -0300

    menu.v2

--- rpg.py
+++ rpg.py
@@ -1,5 +1,7 @@
 # RPG - Vitor Eduardo dos Santos de Oliveira - 2-Info-11
 
+from time import *
+
 # Importação das classes:
 
 from itens.item import Item
@@ -10,10 +12,21 @@ from personagens.monstro import Monstro
 
 # Menu:
 
-print("Bem vindo ao RPyG de Hollow Knight!")
-
-opcao_menu = input("Escolha uma opção: ")
+print("Bem vindo ao RPyG de Star Wars!")
 
 print("1. Iniciar")
 print("2. Criar novo personagem")
 print("3. Sair")
+
+opcao_menu = input("Escolha uma opção: ")
+
+match opcao_menu:
+    case '1':
+        print("iniciar")
+    
+    case '2':
+        pass
+
+    case '3':
+        print("Desligando...")
+        time.sleep(0.2)

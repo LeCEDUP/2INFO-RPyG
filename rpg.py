dce34de514164c4c91d9b270771f3076589d531b
vie2000 <viniciuseduardofranca70@gmail.com>
2025-10-17 08:43:25 -0300

    Pão

--- rpg.py
+++ rpg.py
@@ -1,1 +1,12 @@
-# Desenvolva o seu jogo aqui
+from personagens.heroi import Heroi
+heroi = Heroi("Afera", 300, 20, 30)
+
+from personagens.monstro import Monstro
+Esqueleto = Monstro("Esqueleto", 40, 10, 0, "Morto vivo")
+Soldado_Morto = Monstro("Soldado Morto", 50, 15, 10, "Morto vivo")
+Baú_vivo = Monstro("Baú vivo", 30, 30, 5, "Mimico")
+Cavaleiro_Morto = Monstro("Cavaleiro Morto", 100, 20, 40, "Morto vivo")
+Ogro = Monstro("Ogro", 150, 30, 0, "Floresta")
+Goblin = Monstro("Goblin", 20, 5, 5, "Floresta")
+Aranha_Gigante = Monstro("Aranha Gigante", 80, 25, 10, "Floresta")
+Mago_Negro = Monstro("Mago Negro", 100, 60, 10, "Mago")

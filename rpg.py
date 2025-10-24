949ae88aa5d6f70e0091f7985db290d96258fe6b
juliacaroline12 <jn149575@gmail.com>
2025-10-24 08:26:22 -0300

    Inicializa projeto com classes e menu principal

--- rpg.py
+++ rpg.py
@@ -1,6 +1,5 @@
 import random
 
-# --------- CLASSES ----------
 class Heroi:
     def __init__(self, nome, vida, ataque, defesa):
         self.nome = nome
@@ -9,9 +8,29 @@ class Heroi:
         self.defesa = defesa
         self.itens = []
 
-    def equipar_item(self, item):
-        self.itens.append(item)
-        if isinstance(item, Arma):
-            self.ataque += item.bonus_ataque
-        elif isinstance(item, Armadura):
-            self.defesa += item.defesa
+class Monstro:
+    def __init__(self, nome, vida, ataque, defesa, tipo):
+        self.nome = nome
+        self.vida = vida
+        self.ataque = ataque
+        self.defesa = defesa
+        self.tipo = tipo
+
+class Arma:
+    def __init__(self, nome, dano, bonus_ataque):
+        self.nome = nome
+        self.dano = dano
+        self.bonus_ataque = bonus_ataque
+
+class Armadura:
+    def __init__(self, nome, defesa):
+        self.nome = nome
+        self.defesa = defesa
+
+def menu_principal():
+    print("\n===== MENU PRINCIPAL =====")
+    print("1 - Iniciar Aventura\n2 - Sair\n3 - Ver Introdução")
+    return input("Escolha: ")
+
+
+       

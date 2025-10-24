1cf515509005ccc4e17a055f7e0d6eab89fd01f8
samuelbayma-bit <samuelbayma826@gmail.com>
2025-10-24 08:25:52 -0300

    batalha

--- rpg.py
+++ rpg.py
@@ -29,3 +29,24 @@ def sobre():
     Desenvolvido em Python com orientação a objetos.
     Explore o reino e enfrente o poderoso Dragão Ancião!
     """)
+
+    def batalha(heroi, inimigo, ataques_heroi=1, ataques_inimigo=1):
+    print(f"\n A batalha começou entre {heroi.nome} e {inimigo.nome} ({inimigo.tipo})!")
+
+    while heroi.vida > 0 and inimigo.vida > 0:
+        input(f"\nPressione ENTER para que {heroi.nome} ataque...")
+
+        for _ in range(ataques_heroi):
+            heroi.atacar(inimigo)
+            if inimigo.vida <= 0:
+                print(f"\n {heroi.nome} derrotou o {inimigo.nome}! O reino está salvo!")
+                return
+
+        for _ in range(ataques_inimigo):
+            print(f"\n O {inimigo.nome} prepara um ataque de fogo!")
+            inimigo.atacar(heroi)
+            if heroi.vida <= 0:
+                print(f"\n {heroi.nome} foi derrotado pelo {inimigo.nome}... O reino está perdido.")
+                return
+
+        print(f"\n {heroi.nome}: {heroi.vida} de vida |  {inimigo.nome}: {inimigo.vida} de vida")

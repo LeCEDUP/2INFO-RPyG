d15c2bc545b0dddc09b08676983b1f9013ac827c
nogueira-h <nogueira.info09@gmail.com>
2025-10-24 08:03:50 -0300

    execuçaõi da batalha

--- rpg.py
+++ rpg.py
@@ -50,3 +50,47 @@ heroi.equipar_item(poderes[0])
 heroi.equipar_item(armaduras[0])
 
 print(f"\nVocê escolheu {heroi.nome}!\n")
+
+for inimigo in inimigos:
+    print(f"Batalha contra {inimigo.nome} ---")
+    while heroi.esta_vivo() and inimigo.esta_vivo():
+       
+        print("\nEscolha sua ação:")
+        print("1. Atacar")
+        print("2. Usar item")
+        acao = input("Digite 1 ou 2: ")
+
+        if acao == "1":
+           
+            armas_disponiveis = [item for item in heroi.inventario if isinstance(item, Arma)]
+            print("\nEscolha seu poder:")
+            for i, p in enumerate(armas_disponiveis):
+                print(f"{i+1}. {p.nome} ({p.descricao}, Dano: {heroi.ataque})")
+            poder_escolhido = int(input("Digite o número do poder: ")) - 1
+
+            
+            if 0 <= poder_escolhido < len(armas_disponiveis):
+                heroi.equipar_item(armas_disponiveis[poder_escolhido])
+                heroi.atacar(inimigo)
+            else:
+                print("Escolha inválida! Você perdeu o turno.")
+
+        elif acao == "2":
+            itens_disponiveis = [item for item in heroi.inventario if isinstance(item, Item)]
+            if not itens_disponiveis:
+                print("Você não tem itens para usar!")
+            else:
+                print("\nEscolha um item para usar:")
+                for i, item in enumerate(itens_disponiveis):
+                    print(f"{i+1}. {item.nome} ({item.descricao})")
+                item_escolhido = int(input("Digite o número do item: ")) - 1
+                if 0 <= item_escolhido < len(itens_disponiveis):
+                    item = itens_disponiveis[item_escolhido]
+                    if "30" in item.descricao:
+                        heroi.vida += 30
+                    elif "20" in item.descricao:
+                        heroi.vida += 20
+                    heroi.inventario.remove(item)
+                    print(f"{heroi.nome} usou {item.nome}. Vida atual: {heroi.vida}")
+                else:
+                    print("Escolha inválida! Você perdeu o turno.")

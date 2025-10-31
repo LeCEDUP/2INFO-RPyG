688a4ae0c80b3afde570570d8197fcde3bf10339
JOAO MATHEUS ECCEL CORREA <4553841915@cedup.local>
2025-10-31 08:44:50 -0300

    Adicionei heroi e monstros

---
+++ rpg.py
@@ -0,0 +1,10 @@
+from itens.item import Item
+from itens.arma import Arma
+from itens.armadura import Armadura
+from personagens.heroi import Heroi
+from personagens.monstro import Monstro
+
+Rei_goblin = Monstro("Rei_goblin", 150, 30, 10, "Grande")
+goblin = Monstro("Goblin", 30, 8, 2 "Pequeno")
+Heroi = Heroi("Darwin", 100, 15, 5)
+

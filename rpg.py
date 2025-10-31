bd04385cd2a4146eac999dbcaa1dfe4bc28ffc1e
JOAO MATHEUS ECCEL CORREA <4553841915@cedup.local>
2025-10-31 08:51:15 -0300

    Adicionei armas e armaduras

--- rpg.py
+++ rpg.py
@@ -8,3 +8,6 @@ Rei_goblin = Monstro("Rei_goblin", 150, 30, 10, "Grande")
 goblin = Monstro("Goblin", 30, 8, 2 "Pequeno")
 Heroi = Heroi("Darwin", 100, 15, 5)
 
+
+espada = Arma("Espada Longa", "Uma espada afiada", 10)
+escudo = Armadura("Escudo de Ferro", "Um escudo resistente.", 5)
